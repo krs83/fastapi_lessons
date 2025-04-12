9b30e2f8243e053cc99fa84cb4bf3919eb5a73bb
krs83 <krs83@yandex.ru>
2025-04-12 09:54:31 +0300

    add class Base & model hotels

--- src/database.py
+++ src/database.py
@@ -1,9 +1,12 @@
 
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from src.config import settings
-
+from sqlalchemy.orm import DeclarativeBase
 
 engine = create_async_engine(settings.DB_URL)
 
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 
+
+class Base(DeclarativeBase):
+    pass
