6cd0c6ed35b50f5974b09c6df2a2df16196a9445
krs83 <krs83@yandex.ru>
2025-04-15 21:31:40 +0300

    first migration with alembic

--- src/models/m_hotels.py
+++ src/models/m_hotels.py
@@ -5,7 +5,7 @@ from src.database import Base
 
 
 class HotelsOrm(Base):
-    __table__ = 'hotels'
+    __tablename__ = 'hotels'
 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(length=100))
