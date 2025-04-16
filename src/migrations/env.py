6eaa9cb8810f5e3dccbd274ca7b30733128c3ded
krs83 <krs83@yandex.ru>
2025-04-16 19:19:58 +0300

    rooms migration

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -8,6 +8,7 @@ from alembic import context
 from src.config import settings
 from src.database import Base
 from src.models.m_hotels import HotelsOrm
+from src.models.m_rooms import RoomsOrm
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
